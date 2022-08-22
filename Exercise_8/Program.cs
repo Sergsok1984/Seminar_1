b0c9f43bd0159dc959123e57b8073f8651851762
Сергей Соколов <sergsok1984@gmail.com>
2022-08-22 13:15:58 +0300

    add Exercise 8

---
+++ Exercise_8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i;
+
+for (i = 1; i <= N; i++)
+{
+    if (i % 2 == 0) 
+    {
+        Console.Write(i + ", ");
+    } 
+}
