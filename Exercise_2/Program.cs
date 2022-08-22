673320456befb01bc0496522a9d9624d45ab90be
Сергей Соколов <sergsok1984@gmail.com>
2022-08-22 12:09:18 +0300

    add adjustment

--- Exercise_2/Program.cs
+++ Exercise_2/Program.cs
@@ -3,9 +3,11 @@ int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
 if (a > b)
 {
-    Console.WriteLine($"Наибольшее число: {a}");
+    Console.WriteLine($"Большее число: {a}");
+    Console.WriteLine($"Меньшее число: {b}");
 }
 else 
 {
-    Console.WriteLine($"Наибольшее число: {b}");
+    Console.WriteLine($"Большее число: {b}");
+    Console.WriteLine($"Меньшее число: {a}");
 }
