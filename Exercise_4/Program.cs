0d3dbdf83cd770f54450bbcdfc02a4d11360bd24
Сергей Соколов <sergsok1984@gmail.com>
2022-08-22 12:37:13 +0300

    add Exercise 4

---
+++ Exercise_4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите три числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b && a > c)
+{
+    Console.WriteLine($"Максимальное число: {a}");    
+}
+else if (b > c)
+{
+    Console.WriteLine($"Максимальное число: {b}");   
+}
+else 
+{
+    Console.WriteLine($"Максимальное число: {c}"); 
+}
