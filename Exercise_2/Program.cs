48c91a5b51ce059e27eacac36f92edeb432176bf
Сергей Соколов <sergsok1984@gmail.com>
2022-08-22 12:00:59 +0300

    add Exercise 2

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите два числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine($"Наибольшее число: {a}");
+}
+else 
+{
+    Console.WriteLine($"Наибольшее число: {b}");
+}
